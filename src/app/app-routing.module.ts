67cf4b9c08a7a383a0795d181b491b1eb310e6d5
Alan Duda <alandudatop10@gmail.com>
2021-06-12 22:24:32 -0300

    configure routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { FormsComponent } from './pages/forms/forms.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'forms', component: FormsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
