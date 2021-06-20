9108c17f479203692fc24b3e3fc201dd55cef881
Alan Duda <alandudatop10@gmail.com>
2021-06-20 10:21:32 -0300

    configure routes

--- src/app/pages/contacts/contacts.module.ts
+++ src/app/pages/contacts/contacts.module.ts
@@ -1,13 +1,19 @@
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactsComponent } from './contacts.component';
+
+const routes: Routes = [
+  { path: 'contacts', component: ContactsComponent }
+]
 
 @NgModule({
   declarations: [
     ContactsComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes),
   ]
 })
 export class ContactsModule { }
--- src/app/pages/forms/forms.module.ts
+++ src/app/pages/forms/forms.module.ts
@@ -1,13 +1,19 @@
+import { Routes, RouterModule } from '@angular/router';
 import { FormsComponent } from './forms.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+const routes: Routes = [
+  { path: 'forms', component: FormsComponent }
+]
 
 @NgModule({
   declarations: [
     FormsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ]
 })
 export class FormsModule { }
